8de219c0698cb53a5c44ad013d331d3c6275b0b6
DanielVernon <daniel.vernon.programming@gmail.com>
2018-07-09 15:38:11 +1000

    daniel
    why wont this commit work

---
+++ Assets/Daniel/Scripts/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// What this script is meant to be managing
+/// - pausing game
+/// - respawning player
+/// - keeping track of highscores and stats
+/// - 
+/// </summary>
+public class GameManager : MonoBehaviour {
+    static GameManager manager = new GameManager();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
